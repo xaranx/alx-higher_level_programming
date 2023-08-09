b246b773f3396cb04a0ff070307bc5bc385da6da
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-09 16:07:47 -0700

    14 smill

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for c in range(25, -1, -1):
-    if c % 2 == 0:
-        print(chr(65 + c), end="")
-    else:
-        print(chr(97 + c), end="")
-
+for c in range(122, 96, -1):
+    c = c - 32 if c % 2 else c
+    print("{}".format(chr(c)), end="")
