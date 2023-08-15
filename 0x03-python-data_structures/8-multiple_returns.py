a4319faca763f769081362c2cf94dcad30cd3f7f
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-15 14:40:47 -0700

    more return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length == 0:
-        return (None)
-    first = sentence[0]
-    return length, first
+    if sentence:
+        first = sentence[0]
+    else:
+        return None
+    return (length, first)
