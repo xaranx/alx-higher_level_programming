802315df42071c14101520d5b5171a8ca7df9389
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-15 14:29:23 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return (None, 0)
+        return (None)
     first_char = sentence[0]
     return (len(sentence), first_char)
