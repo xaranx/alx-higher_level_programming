0dc09ac0a8df859742ee22bd49cab0b073f92db8
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-16 13:43:12 -0700

    common string

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_elem = set(set_1)
-    for str in set_2:
-        if str in common_elem:
-            common_elem.add(str)
-            return str
+    common_str = set()
+    for str in set_1:
+        if str in set_2:
+            common_str.add(str)
+    return common_str
