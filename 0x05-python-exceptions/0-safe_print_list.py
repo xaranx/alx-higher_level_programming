2fdeac3efa847c91b736f4e23a78fd4d255b210e
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-28 14:07:50 -0700

    safe list print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
+
+
+if __name__ == "__main__":
+    safe_print_list = __import__('0-safe_print_list').safe_print_list
+
+    my_list = [1, 2, 3, 4, 5]
+    nb_print = safe_print_list(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+    nb_print = safe_print_list(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
