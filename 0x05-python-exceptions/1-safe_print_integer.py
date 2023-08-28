05d298923b4ca76ea1066de1f98d8235605eeff8
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-28 14:38:06 -0700

    1. Safe printing of an integers list

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError):
+        return False
+
+
+if __name__ == "__main__":
+    safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+
+    value = 89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
