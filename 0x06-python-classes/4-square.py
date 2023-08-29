fa5b82c1df9339055c010c79e202ea9794a47a0c
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-29 16:23:51 -0700

    4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+
+"""Module define square class"""
+
+
+class Square:
+
+    """private instance attr type int"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    """retrieve private attr"""
+
+    @property
+    def size(self):
+        return self.__size
+
+    """set value to private attr"""
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
