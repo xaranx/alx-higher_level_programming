c39acc80afef19e978c34cb702228cc02aa5e6c3
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-29 14:51:46 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This module defines a Square class based on 0-square.py"""
+
+
+class Square:
+
+    """Private instance attribute: size"""
+
+    def __init__(self, size):
+        """private attr with double underscore"""
+        self.__size = size
