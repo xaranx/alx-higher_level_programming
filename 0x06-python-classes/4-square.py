c53ef4a535a719bf6bdff5b302cfb358a4ba5d52
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-29 16:41:56 -0700

    test 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,50 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-
-"""Module define square class"""
+"""This module defines a Square class"""
 
 
 class Square:
+    """
+    Class that defines a square
 
-    """private instance attr type int"""
+    Attributes:
+        size (int): size of the square
+    """
 
     def __init__(self, size=0):
+        """
+        Init method is a constructor for Square class
+        Args:
+            size: size of the square
+        """
         self.__size = size
-
-    """retrieve private attr"""
 
     @property
     def size(self):
+        """
+        Public instance method that returns the current square size
+        """
         return self.__size
-
-    """set value to private attr"""
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """
+        Public instance method that returns the current square size
+        Args:
+            value: size of the square
+
+        Returns:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if isinstance(value, int) is False:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
+        """
+        Public instance method that returns the current square area
+        """
         return self.__size ** 2
