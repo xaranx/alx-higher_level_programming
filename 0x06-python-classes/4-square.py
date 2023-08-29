3e0d813b8abb656cb30be79d0ee57e7a156a8f4f
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-08-29 16:38:33 -0700

    test 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,7 +23,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
