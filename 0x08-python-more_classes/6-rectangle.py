97af4481344fd26c15e33b213dad3654a83ce3e5
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-04 15:42:37 -0700

    6. How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,12 +5,15 @@
 
 class Rectangle:
 
-    number_of_instances = 0
     """class that defines a Rectangle"""
+    
+    number_of_instances = 0
+
     """Private instance attribute: width"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        
         Rectangle.number_of_instances += 1
 
     @property
