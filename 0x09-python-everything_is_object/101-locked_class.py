4a5bfdddc261508a474cc35b517665c8276c6078
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-05 15:41:01 -0700

    101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,9 +9,10 @@ class LockedClass:
     """
     __slots__ :attr restricts theinstance attr
     only those list as in this case "first_name"
+    """
 
     __slots__ = ["first_name"]
-    """
+
 
     def __init__(self, first_name=""):
         self.first_name = first_name
