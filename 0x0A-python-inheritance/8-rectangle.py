bac5975e5599bf553fbfcb6ad15ccf202b4b14f1
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-11 15:57:23 -0700

    8. Reactangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 
 """
-class modul for BaseGeometry
-"""
-
-
-class BaseGeometry():
-    """public instance methode"""
-    def area(self):
-        raise Exception("area() is not implemented")
-    """public instance method that validate value"""
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""
 class module that inherite from BaseGeometry
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
+        """
+        initialize an instance of the rectangle class
+        """
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
