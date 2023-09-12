71b874a0a696d30b772906a2ad295e98f18ab6df
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-12 14:34:33 -0700

    print json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+Module of Json string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Methode that returns the JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
