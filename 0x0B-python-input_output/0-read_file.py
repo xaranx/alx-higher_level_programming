0b8d0393e0f3e2cfadc6389e41dc22b60cdc5ab0
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-12 11:53:24 -0700

    read txt

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     """
     Methode for Read line
     """
-    with open("my_file_0.txt", encoding="utf-8") as f:
+    with open("filename", encoding="utf-8") as f:
         print(f.read(), end="")
