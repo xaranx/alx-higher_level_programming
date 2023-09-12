d3159a45851155ed0a606a9b99e5b9cda47a444d
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-12 15:00:30 -0700

    json loads

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Method from json string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Methode represent json by str
+    """
+    return json.loads(my_str)
