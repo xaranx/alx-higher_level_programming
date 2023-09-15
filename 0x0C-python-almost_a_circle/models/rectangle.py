709e5b25ab97bcfcd56286921181c96ee6c97576
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-15 10:40:50 -0700

    2, first rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
