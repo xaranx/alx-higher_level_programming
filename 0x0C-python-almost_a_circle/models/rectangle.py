ec0a04e38ff46ec3714fb0d6ebf89cfd32b848ee
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-15 16:04:45 -0700

    display stdout #

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,3 +62,9 @@ class Rectangle(Base):
     def area(self):
         r = self.__width * self.__height
         return r
+
+    def display(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                print("#", end="")
+            print()
