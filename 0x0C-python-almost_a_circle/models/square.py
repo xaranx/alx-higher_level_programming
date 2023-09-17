3a115e6ae029049c0fef15db2da2c6029953b08b
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-17 15:05:09 -0700

    12. Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,11 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ constructore """
         super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        """Str method"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
@@ -18,3 +23,12 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
