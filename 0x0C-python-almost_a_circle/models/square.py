50e049d0176a71ed7a00603b7266feb702de6e5f
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-09-17 14:33:08 -0700

    11. Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,3 +9,12 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ constructore """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
