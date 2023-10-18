95cb140bfb676b2a75271777e120aab6c567cec1
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-10-18 15:02:47 -0700

    100 101 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,7 @@
    -- Results must be sorted in ascending order by the genre name
    -- The database name will be passed as an argument of the mysql command
 
-SELECT name from tv_genres
+SELECT name FROM tv_genres
 JOIN tv_show_genres ON id=tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
