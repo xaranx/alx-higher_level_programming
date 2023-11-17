03ee4f217c77f92f46d451c9fdf1e0ba09585441
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-11-17 15:25:54 -0800

    task 2, 3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Module that  takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument """
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """function that  takes in an argument and displays all values
+    in the states table of hbtn_0e_0_usa where name matches the argument"""
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+            ORDER BY id ASC".format(sys.argv[4]))
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
