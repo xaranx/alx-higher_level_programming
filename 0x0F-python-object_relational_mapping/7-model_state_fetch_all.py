0c1d84d04c322ba6b8ef03eef63809a3cb8a38a4
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-11-19 05:29:01 -0800

    task 6, 7 update

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+""" Lists all State objects from the database hbtn_0e_6_usa """
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""class state"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,6 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """properties of the states"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
