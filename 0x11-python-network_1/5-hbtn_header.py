c9d7ac8db8c0367e8b08fa4f6005206709bed864
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2023-12-01 13:19:25 -0800

    task 4, 5, 6

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    print(res.headers.get('X-Request-Id'))
