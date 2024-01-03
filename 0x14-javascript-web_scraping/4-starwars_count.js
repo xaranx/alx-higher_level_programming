734d1e59feeaa9a2987f89c35e1d28d59658f44f
ARIF BOUCHAIB <arrif.bouchaib03@gmail.com>
2024-01-03 10:10:23 -0800

    task 7

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,6 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
-  
   const nb = JSON.parse(body).results.filter((elem) => {
     return elem.characters.filter((url) => { return url.includes('18'); }).length;
   }).length;
